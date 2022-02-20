1bcb2df373e21f47619fa9eba89de572c810652a
Khaled Tangao <ktangao@gmail.com>
2022-02-20 11:36:54 +0100

    fizzbuzz: add fizzbuzz sequence generator
    Introduce a class allowing to generate a fizzbuzz sequence.
    Add tests accordingly.

---
+++ lib/fizzbuzz.py
@@ -0,0 +1,59 @@
+
+__all__ = (
+	"FizzBuzz",
+)
+
+class FizzBuzzSeqGenerator:
+	"""
+	FizzBuzz Sequence generator.
+
+	Use as following:
+		fizzbuzz = FizzBuzz(3, 5, 100, "fizz", "buzz")
+		seq = fizzbuzz.sequence()
+	"""
+	
+	def __init__(self, int1, int2, limit, str1, str2):
+		self.int1 = int1
+		self.int2 = int2
+		self.limit = limit
+		self.str1 = str1
+		self.str2 = str2
+		self._validate()
+
+	def _validate(self):
+		try:
+			self.int1 = int(self.int1)
+			self.int2 = int(self.int2)
+			self.limit = int(self.limit)
+		except ValueError:
+			raise ValueError("int1, int2 and limit must be integers")
+
+		if not self.int1 or not self.int2:
+			raise ValueError(f"int1 and int2 cannot be null")
+		if self.limit <= 1:
+			raise ValueError(f"the provided limit ({self.limit}) "
+                             "cannot be lower or equal to 1")
+		if self.int1 > self.limit or self.int2 > self.limit:
+			raise ValueError(f"int1 ({self.int1}) and int2 ({self.int2}) "
+                             f"cannot be bigger than the limit ({self.limit})")
+		if not isinstance(self.str1, str) or not isinstance(self.str2, str):
+			raise ValueError(f"str1 and str2 must be of type string")
+
+	def sequence(self):
+		res = ""
+		for pos in range(1, self.limit + 1):
+			replaced = False
+			if not (pos % self.int1):
+				res += self.str1
+				replaced = True
+			if not (pos % self.int2):
+				res += self.str2
+				replaced = True
+			if not replaced:
+				res += f"{pos}"
+
+			res += ","
+
+		# remove the trailing ","
+		return res[:-1]
+			

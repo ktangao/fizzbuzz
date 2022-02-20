ee177ba2bb6996eca50725a387e525a37d5bc067
Khaled Tangao <ktangao@gmail.com>
2022-02-20 13:28:09 +0100

    fizzbuzz: introduce a tornado server
    The server is listing to the port provided by the environment variable
    FIZZBUZZ_SERVER_PORT.
    
    When all the expected provided arguments are valid an HTTP 200 reply is
    returned with the following body:
    
    	$ curl -X POST 0.0.0.0:8888/fizzbuzz/sequence --header "Content-Type:application/json" --data @data.json
    	> {"sequence": "1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz,16,17,fizz,19,buzz"}
    
    where data.json contains the following information:
      {
    	"int1": 3,
    	"int2": 5,
    	"limit": 20,
    	"str1": fizz,
    	"str2": buzz
      }
    
    On error the bellow response is returned instead with 400 as HTTP status code:
    
      > {"error": "int2 is missing in the request body"}

--- lib/test_fizzbuzz.py
+++ lib/test_fizzbuzz.py
@@ -3,40 +3,40 @@
 
 import unittest
 
-from fizzbuzz import FizzBuzz
+from fizzbuzz import FizzBuzzSeqGenerator
 
-class TestFizzBuzz(unittest.TestCase):
+class TestFizzBuzzSeqGenerator(unittest.TestCase):
 	def test_int_as_invalid_str(self):
 		want = "int1, int2 and limit must be integers"
 		for (int1, int2, limit) in [("invalid", 5, 10),
 								    (5, "invalid", 10),
                                     (3, 5, "invalid")]:
 			with self.assertRaisesRegex(ValueError, want):
-				FizzBuzz(int1, int2, limit, "fizz", "buzz")
+				FizzBuzzSeqGenerator(int1, int2, limit, "fizz", "buzz")
 
 	def test_null_integer(self):
 		want = "int1 and int2 cannot be null"
 		for (int1, int2) in [(0, 3), (3, 0)]:
 			with self.assertRaisesRegex(ValueError, want):
-				FizzBuzz(int1, int2, 10, "fizz", "buzz")
+				FizzBuzzSeqGenerator(int1, int2, 10, "fizz", "buzz")
 
 	def test_limit_lower_1(self):
 		want = "the provided limit (.*) cannot be lower or equal to 1"
 		for limit in [-1, 0, 1]:
 			with self.assertRaisesRegex(ValueError, want):
-				FizzBuzz(3, 5, limit, "fizz", "buzz")
+				FizzBuzzSeqGenerator(3, 5, limit, "fizz", "buzz")
 
 	def test_int1_bigger_than_limit(self):
 		want = "int1 (.*) and int2 (.*) cannot be bigger than the limit (.*)"
 		for (int1, int2) in [(3, 5), (5, 3)]:
 			with self.assertRaisesRegex(ValueError, want):
-				FizzBuzz(int1, int2, 4, "fizz", "buzz")
+				FizzBuzzSeqGenerator(int1, int2, 4, "fizz", "buzz")
 
 	def test_str1_str2_not_string(self):
 		want = "str1 and str2 must be of type string"
 		for (str1, str2) in [("fizz", 2), (2, "buzz")]:
 			with self.assertRaisesRegex(ValueError, want):
-				FizzBuzz(3, 5, 100, str1, str2)
+				FizzBuzzSeqGenerator(3, 5, 100, str1, str2)
 
 	def test_sequence(self):
 		for (int1, int2, limit, str1, str2) in [
@@ -44,7 +44,7 @@ class TestFizzBuzz(unittest.TestCase):
 			(3, 5, 20, "fizz", "buzz"),
 			("03", "05", "020", "fizz", "buzz")
 		]:
-			fizzbuzz = FizzBuzz(int1, int2, limit, str1, str2)
+			fizzbuzz = FizzBuzzSeqGenerator(int1, int2, limit, str1, str2)
 			seq = fizzbuzz.sequence()
 			want = "1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz,16,17,fizz,19,buzz"
 			self.assertEqual(seq, want)
---
+++ server.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os 
+import logging
+
+from tornado.escape import (json_decode, json_encode)
+from tornado.ioloop import IOLoop
+from tornado.web import (RequestHandler, Application)
+
+from lib.fizzbuzz import FizzBuzzSeqGenerator
+
+logging.basicConfig(format="%(name)s: %(asctime)s: %(levelname)s: %(message)s")
+LOGGER = logging.getLogger("fizzbuzz")
+LOGGER.setLevel(logging.INFO)
+
+# {{{ FizzBuzz Request handler
+
+class FizzBuzzHandler(RequestHandler):
+	CONTENT_TYPES = ["application/json", "application/x-json"]
+	REPLY_CONTENT_TYPE = "application/json; charset=UTF-8"
+	HTTP_BAD_REQ_CODE = 400
+	HTTP_STATUS_OK = 200
+	
+	def initialize(self):
+		self.error = None
+
+	def prepare(self):
+		if not self._check_content_type():
+			return
+		self.retrievedArgs = {}
+		for key in ["int1", "int2", "limit", "str1", "str2"]:
+			body = json_decode(self.request.body)
+			val = body.get(key)
+			if val is None:
+				self.error = {
+					"code": self.HTTP_BAD_REQ_CODE,
+					"msg": f"{key} is missing in the request body"
+				}
+				return
+			self.retrievedArgs[key] = str(val).strip()
+
+	def post(self):
+		if self.error is not None:
+			self._reply_error_and_finish()
+			return
+		try:
+			seqGenerator = FizzBuzzSeqGenerator(**self.retrievedArgs)
+		except ValueError as err:
+			self.error = {
+				"code": self.HTTP_BAD_REQ_CODE,
+				"msg": str(err),
+			}
+			self._reply_error_and_finish()
+			return
+		self._reply_success(seqGenerator.sequence())
+
+	# {{{ Private helpers
+
+	def _check_content_type(self):
+		content_type = self.request.headers.get("Content-Type")
+		if content_type is None:
+			self.error = {
+				"code": self.HTTP_BAD_REQ_CODE,
+				"msg": "invalid Content-Type",
+			}
+			return False
+		content_type = content_type.lstrip()
+		for expected_ctype in self.CONTENT_TYPES:
+			if content_type.startswith(expected_ctype):
+				return True
+		self.error = {
+			"code": self.HTTP_BAD_REQ_CODE,
+			"msg": (f"accepted content types are: {self.CONTENT_TYPES}, "
+                    f" got {content_type}"),
+		}
+		return False
+
+	def _set_reply_content_type(self):
+		self.set_header("Content-Type", self.REPLY_CONTENT_TYPE)
+
+	def _reply_error_and_finish(self):
+		assert (self.error)
+		self._set_reply_content_type()
+		self.set_status(self.error.get("code"))
+		self.finish(json_encode({"error": self.error.get("msg")}))
+		LOGGER.info("error on request: %s", self.error)
+
+	def _reply_success(self, sequence):
+		self._set_reply_content_type()
+		self.set_status(self.HTTP_STATUS_OK)
+		self.finish(json_encode({"sequence": sequence}))
+		LOGGER.info(f"successfull sequence generated for: %s", self.retrievedArgs)
+		
+	# }}}
+# }}}
+
+def app():
+	return Application([
+        (r"/fizzbuzz/sequence", FizzBuzzHandler),
+    ])
+	
+if __name__ == "__main__":
+	port = os.getenv("FIZZBUZZ_SERVER_PORT", "8888")
+	app().listen(int(port))
+	LOGGER.info("server started")
+	IOLoop.current().start()
